89f7d2ef359ce3d49dc486292bb1b44ce9997516
Dana Kaplan <danakaplan96@gmail.com>
2023-12-01 14:52:53 -0500

    JSON Format and Toggle Functionality Working > Co-authored-by: Nancy <nancyhuang2013@gmail.com> Co-authored-by: Brian <bnlee419@gmail.com> Co-authored-by: Dana <danakaplan96@gmail.com> Co-authored-by: Adrian <adrianinsingo@gmail.com> Co-authored-by: William <william.b.kil@gmail.com>

--- src/client/components/StateSnapshots.jsx
+++ src/client/components/StateSnapshots.jsx
@@ -11,7 +11,7 @@ const StateSnapshots = () => {
   const [updatedRenderedSnapshots, setUpdatedRenderedSnapshots] = useState([]);
 
   // To handle toggling to JSON format display
-  const [showAsJSON, setShowAsJSON] = useState(false);
+  const [showAsJSON, setShowAsJSON] = useState(true); // Set to true for default JSON view
 
   // useEffect so that we get any updates when a snapshot is added to the stateSnapshotArray in the store
   useEffect(() => {
@@ -38,12 +38,6 @@ const StateSnapshots = () => {
                 enableClipboard={false}
               />
             ) : (
-              // <li
-              //   className='bg-white bg-opacity-20 hover:bg-opacity-40 rounded-lg p-2 mt-2 mb-2'
-              //   key={uuid()}
-              // >
-              //   {JSON.stringify(el.stateSnapshot)}
-              // </li>
               Object.keys(el.stateSnapshot).map((key) => (
                 <li
                   className='bg-white bg-opacity-20 hover:bg-opacity-40 rounded-lg p-2 mt-2 mb-2'
@@ -69,16 +63,16 @@ const StateSnapshots = () => {
       className='flex flex-col border-t-2 border-lt-grey w-full m-auto'
       style={{ height: '93vh', overflow: 'auto' }}
     >
-      <label class='relative inline-flex items-center mb-5 cursor-pointer'>
+      <label className='relative inline-flex items-center mb-5 cursor-pointer'>
         <input
           type='checkbox'
           value=''
-          class='sr-only peer'
+          className='sr-only peer'
           onChange={handleToggleChange}
-        ></input>
-        <div class="w-9 h-5 peer-focus:outline-white peer-focus:ring-4 dark:peer-focus:ring-blue rounded-full peer dark:bg-lt-grey peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-lt-grey after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-lt-grey peer-checked:bg-blue"></div>
-        <span class='ms-3 text-sm font-medium text-white dark:text-white'>
-          JSON Format
+        />
+        <div className="w-9 h-5 peer-focus:outline-white peer-focus:ring-4 dark:peer-focus:ring-blue rounded-full peer dark:bg-lt-grey peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-lt-grey after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-lt-grey peer-checked:bg-blue"></div>
+        <span className='ms-3 text-sm font-medium text-white dark:text-white'>
+          Show As List
         </span>
       </label>
 
