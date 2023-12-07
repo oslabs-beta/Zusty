b20a78b30d75fa42506e285e368a11f2dba356fc
Dana Kaplan <danakaplan96@gmail.com>
2023-12-07 13:58:15 -0500

    styling store

--- src/client/components/StateSnapshots.jsx
+++ src/client/components/StateSnapshots.jsx
@@ -15,55 +15,56 @@ const StateSnapshots = () => {
 
   return (
     <div
-      className="flex flex-col border-t-2 border-lt-grey w-full m-auto"
+      className='flex flex-col border-t-2 border-lt-grey w-full m-auto'
       style={{ height: '93vh', overflow: 'auto' }}
     >
       {/* Toggle Switch */}
-      <label className="relative inline-flex items-center mb-2 mt-2 ml-2 cursor-pointer">
+      <label className='relative inline-flex items-center mb-2 mt-2 ml-2 cursor-pointer'>
         <input
-          type="checkbox"
-          value=""
-          className="sr-only peer"
+          type='checkbox'
+          value=''
+          className='sr-only peer'
           onChange={handleToggleChange}
         />
         <div className="w-9 h-5 border-2 border-white peer-focus:outline-white peer-focus:ring-4 dark:peer-focus:ring-blue rounded-full peer dark:bg-lt-grey peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-lt-grey after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-lt-grey peer-checked:bg-blue"></div>
-        <span className="ms-3 text-sm font-medium text-white dark:text-white">
+        <span className='ms-3 text-sm font-medium text-white dark:text-white'>
           Show As List
         </span>
       </label>
       {/* State Snapshot Section */}
-      <h1 className="text-center text-xl font-bold text-white my-5 mt-0 mb-0">
+      <h1 className='text-center text-xl font-bold text-white my-5 mt-0 mb-0'>
         State Snapshots
       </h1>
       {/* Map through snapshots to display in either JSON format or list view depending on toggle switch */}
-      <div className="flex flex-col justify-center items-center gap-1.5">
+      <div className='flex flex-col justify-center items-center gap-1.5'>
         {stateSnapshotArray.map((el) => (
           <div
             key={uuid()}
-            className="flex flex-col justify-center item-center text-left p-2 bg-code-bg text-white rounded-md my-2 w-11/12 overflow-auto backdrop-opacity-30	hover:backdrop-opacity-100 border border-gray-200"
+            className='flex flex-col justify-center item-center text-left p-2 bg-code-bg text-white rounded-md my-2 w-11/12 overflow-auto backdrop-opacity-30	hover:backdrop-opacity-100 border border-gray-200'
           >
             <ul>
               <li
-                className="bg-white bg-opacity-20 hover:bg-opacity-40 rounded-lg p-2 mt-2 mb-2"
+                className='bg-white bg-opacity-20 hover:bg-opacity-40 rounded-lg p-2 mt-2 mb-2'
                 key={uuid()}
               >
-                <span className="font-bold mr-2 text-white">Timestamp:</span>
+                <span className='font-bold mr-2 text-white'>Timestamp:</span>
                 {`${new Date(el.timestamp).toLocaleString()}`}
               </li>
               {showAsJSON ? (
                 <ReactJson
                   src={el.stateSnapshot}
-                  theme="hopscotch"
+                  theme='hopscotch'
                   displayDataTypes={false}
                   enableClipboard={false}
+                  quotesOnKeys={false}
                 />
               ) : (
                 Object.keys(el.stateSnapshot).map((key) => (
                   <li
-                    className="bg-white bg-opacity-20 hover:bg-opacity-40 rounded-lg p-2 mt-2 mb-2 text-white"
+                    className='bg-white bg-opacity-20 hover:bg-opacity-40 rounded-lg p-2 mt-2 mb-2 text-white'
                     key={uuid()}
                   >
-                    <span className="font-bold mr-2 text-white">{`${key}:`}</span>
+                    <span className='font-bold mr-2 text-white'>{`${key}:`}</span>
                     {`${JSON.stringify(el.stateSnapshot[key])}`}
                   </li>
                 ))
--- src/client/components/Store.jsx
+++ src/client/components/Store.jsx
@@ -6,14 +6,24 @@ const Store = () => {
   const { store } = useStore();
 
   return (
-    <div>
-      <ReactJson
-        src={store}
-        theme='hopscotch'
-        displayDataTypes={false}
-        enableClipboard={false}
-      />
-    </div>
+    <>
+      <p className='text-center text-xl font-bold text-white my-5 mt-3 mb-0'>
+        Store:
+      </p>
+      <div
+        className='flex justify-center align-center h-10/12'
+        style={{ overflowY: 'auto', height: '93vh' }}
+      >
+        <ReactJson
+          src={store}
+          theme='hopscotch'
+          displayDataTypes={false}
+          enableClipboard={false}
+          quotesOnKeys={false}
+          style={{ fontSize: '12px' }}
+        />
+      </div>
+    </>
   );
 };
 
